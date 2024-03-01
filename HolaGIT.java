43f4ceae39e1f072f7db1ae408be47a362486638
JesusFPCadiz <jesusantonio.patricio.vico@iesfernandoaguilar.es>
2024-03-01 10:20:05 +0100

    Nuevo mensaje commit

--- HolaGIT.java
+++ HolaGIT.java
@@ -2,5 +2,6 @@ public class HolaGIT{
 	public static void main(String[] args){
 		System.out.println("Hola, usando GIT");		
 		System.out.println("Modificacion");
+		System.out.println("Otra modificacion");
 	}
 }
